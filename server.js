c0fee481cc69b8b0aa9f984ae325002495a0376c
Gael Zouta <gaelzouta@gmail.com>
2026-05-18 12:26:27 +0000

    Fix stats waitress sales

--- server.js
+++ server.js
@@ -935,23 +935,24 @@ app.get("/api/stats", async (req, res) => {
     LIMIT 10
   `);
 
-  const waitressSales = await query(`
-  SELECT waitress_name, COALESCE(SUM(total),0)::int AS total
+ const waitressSales = await query(`
+  SELECT waitress_name,
+  COALESCE(SUM(total),0)::int AS total
   FROM invoices
   WHERE status='paid'
   GROUP BY waitress_name
   ORDER BY total DESC
 `);
 
-  res.json({
-    day: day.rows[0].total,
-    month: month.rows[0].total,
-    unpaid: unpaid.rows[0].c,
-    lowStock: low.rows[0].c,
-    topProducts: top.rows
-    waitressSales: waitressSales.rows,
+res.json({
+  day: day.rows[0].total,
+  month: month.rows[0].total,
+  unpaid: unpaid.rows[0].c,
+  lowStock: low.rows[0].c,
+  topProducts: top.rows,
+  waitressSales: waitressSales.rows
+}); 
 
-  });
 });
 
 app.get("*", (req, res) => {
